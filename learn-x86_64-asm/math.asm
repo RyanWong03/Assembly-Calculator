e6f20fc18b6a5b943ea7d3b81654ab08a395b8de
Ryan <ryanwong935@gmail.com>
2024-11-30 22:35:22 -0500

    User able to enter in first number, operation, and second number.

--- learn-x86_64-asm/math.asm
+++ learn-x86_64-asm/math.asm
@@ -1,96 +1,151 @@
 	;; Program to practice mathematical operations in x86-64 assembly.
 	global _start
 	section .bss
-sum_buffer:	resb 10		;Reserve 10 bytes of uninitialized data
+sum_buffer:	resb 22
 
+	section .data
+main_prompt:	db "**********Welcome to the x86-64 asm calculator.*********", 0xA
+	db "To use this calculator, enter in your first number, and press Enter.", 0xA
+	db "Then enter in your operation (+, -), and press Enter.", 0xA
+	db "Finally, enter in your second number and press Enter.", 0xA
+	db "The result of num1 op num2 will be displayed.", 0xA, 0
+main_prompt_len:	equ $ - main_prompt
+
+	;;22 bytes because the largest signed 64-bit number is 19 digits long. Add one for the negative sign, one for the enter key, and one for the NULL byte.
+first_number_buffer:	db 22 dup (0)
+operation_buffer:	db 3 dup (0)
+second_number_buffer:	db 22 dup (0)
+
+result_str:	db "The result is: ", 0
+result_str_len: equ $ - result_str
+
+	;; File descriptors
+stdin:	equ 0x0
+stdout:	equ 0x1
+stderr:	equ 0x2
+
+result:		db 22 dup (0)
 	section .text
+	
 _start:
-	;; Adding two numbers where they are in registers already
-	mov rdi, 15
-	mov rsi, 10
-	add rdi, rsi		;rdi += rsi (15 + 10 = 25)
+	CALL main
+	RET 
 
-	;; Before we print the integer, it needs to be converted to a string first.
-	mov rdi, rdi		;Does nothing, but just want to show usage of loading integer value into rdi as the first argument.
-	lea rsi, [sum_buffer]	;Load the effective address of sum_buffer into rsi.
-	call int2string		;Call the subroutine, converting the integer in rdi into a string and storing it in memory at the address in rsi.
+	;; My main subroutine. I don't like _start lol.
+main:	
+	;; Output the main prompt, which gets displayed at the start of the program.
+	LEA rsi, [main_prompt]
+	MOV rdx, main_prompt_len
+	CALL output_string
 
-	mov rax, 1		;Write system call. We want to write the sum to stdout
-	mov rdi, 1		;File descriptor (fd) for stdout is 1.
-	;Buffer to output is already stored in rsi.
-	mov rdx, 2		;"25" has 2 characters.
-	syscall
+	;; Get the user's first number.
+	MOV rdi, stdin
+	LEA rsi, [first_number_buffer]
+	MOV rdx, 22
+	CALL read_string
 
-	mov rax, 60		;Exit system call code.
-	mov rdi, 0		;Exit(0) basically.
-	syscall
+	;; Get the user's operation.
+	MOV rdi, stdin
+	LEA rsi, [operation_buffer]
+	MOV rdx, 3
+	CALL read_string
 
+	;; Get the user's second number.
+	MOV rdi, stdin
+	LEA rsi, [second_number_buffer]
+	MOV rdx, 22
+	CALL read_string
+
+	MOV rax, 60		;Exit system call code.
+	MOV rdi, 0		;Exit(0) basically.
+	SYSCALL
+
+	;; read_string reads a user input string from a file (fd passed into rdi) and stores it in memory at the pointer passed into rsi.
+	;; This also takes in a parameter that determines how many characters to read (passed into rdx).
+read_string:
+	MOV rax, 0		;Code for system call read
+	SYSCALL
+	RET
+
+	;; output_string outputs a string to stdout. The pointer to the string is passed inro rsi. The length of the string must also be passed into rdx.
+output_string:
+	MOV rdi, stdout
+	MOV rax, 1		;Code for write system call.
+	SYSCALL
+	RET
+	
+	;; Subroutine to add two numbers (passed into rdi and rsi), result passed into rax.
+adding:
+	ADD rdi, rsi
+	MOV rax, rdi
+	RET
+	
 int2string:			;Converts an integer (passed into rdi) to a string and stores it into the input buffer (passed into rsi).
-	push rsi		;Push rsi onto the stack, so we can restore it before we return.
+	PUSH rsi		;Push rsi onto the stack, so we can restore it before we return.
 
 	;; If integer is 0, it's not negative.
-	cmp rdi, 0
-	jge i2s_positive
+	CMP rdi, 0
+	JGE i2s_positive
 
 i2s_negative:
-	mov dl, 0x2D		;ASCII minus '-'
-	mov byte [rsi], dl	;Store minus sign into buffer.
-	add rsi, 1		;Increment buffer pointer by 1 byte.
-	neg rdi			;Take's two's complement of rdi and stores it back in rdi. RSB equivalent in ARM.
+	MOV dl, 0x2D		;ASCII minus '-'
+	MOV byte [rsi], dl	;Store minus sign into buffer.
+	ADD rsi, 1		;Increment buffer pointer by 1 byte.
+	NEG rdi			;Take's two's complement of rdi and stores it back in rdi. RSB equivalent in ARM.
 
 i2s_positive:
-	mov rdx, 0		;Holds number of digits.
-	mov rcx, 10		;Will be used for multiplication and division later.
+	MOV rdx, 0		;Holds number of digits.
+	MOV rcx, 10		;Will be used for multiplication and division later.
 
 	;If zero, store 0 in buffer and return.
-	cmp rdi, 0
-	je i2s_zero
+	CMP rdi, 0
+	JE i2s_zero
 
 i2s_digits:
 	;; If integer is 0, store the digits into the input buffer in reverse order.
-	cmp rdi, 0
-	je i2s_reverse_digits
+	CMP rdi, 0
+	JE i2s_reverse_digits
 
 	;Dividing rdi/rcx, quotient goes into r8.
-	push rdx
-	mov rdx, 0 		;Top 64 bits of dividend go into rdx.
-	mov rax, rdi		;Bottom 64 bits of dividend go into rax.
+	PUSH rdx
+	MOV rdx, 0 		;Top 64 bits of dividend go into rdx.
+	MOV rax, rdi		;Bottom 64 bits of dividend go into rax.
 	;; Divisor is already in rcx, from i2s_positive, which is the correct register.
-	div rcx			;rdx:rax (rdi) / rcx; Quotient stored in rax.
-	pop rdx
+	DIV rcx			;rdx:rax (rdi) / rcx; Quotient stored in rax.
+	POP rdx
 
 	;; MLS r8, rax, rcx, rdi equivalent in ARM
-	push rax
-	imul rax, rcx
-	push rdi
-	sub rdi, rax
-	mov r8, rdi
-	pop rdi
-	pop rax
+	PUSH rax
+	IMUL rax, rcx
+	PUSH rdi
+	SUB rdi, rax
+	MOV r8, rdi
+	POP rdi
+	POP rax
 
-	push r8
-	mov rdi, rax
-	add rdx, 1
-	jmp i2s_digits
+	PUSH r8
+	MOV rdi, rax
+	ADD rdx, 1
+	JMP i2s_digits
 
 i2s_zero:
-	mov dl, 0x30
-	mov byte [rsi], dl	;Store 0 in buffer
-	add rsi, 1
-	jmp i2s_end
+	MOV dl, 0x30
+	MOV byte [rsi], dl	;Store 0 in buffer
+	ADD rsi, 1
+	JMP i2s_end
 
 i2s_reverse_digits:
-	pop r8
-	add r8b, 0x30
-	mov byte [rsi], r8b	;Store characterized number into input buffer
-	add rsi, 1
-	sub rdx, 1		;Decrement digit counter
-	cmp rdx, 0
-	jne i2s_reverse_digits	;If there are digits remaining, continue
+	POP r8
+	ADD r8b, 0x30
+	MOV byte [rsi], r8b	;Store characterized number into input buffer
+	ADD rsi, 1
+	SUB rdx, 1		;Decrement digit counter
+	CMP rdx, 0
+	JNE i2s_reverse_digits	;If there are digits remaining, continue
 
 i2s_end:
 	;; Store null terminator at end of string.
-	mov dl, 0
-	mov byte [rsi], dl
-	pop rsi			;Restore pointer address.
-	ret
+	MOV dl, 0
+	MOV byte [rsi], dl
+	POP rsi			;Restore pointer address.
+	RET
